073f219726a785d5575e7355801ea8498236ec03
Riccardo <r.cannella.90@gmail.com>
2025-10-26 13:16:18 +0100

    feat(tf): import lxc template and add semaphore lxc

---
+++ terraform/lxc_alpine-lxc-template.tf
@@ -0,0 +1,62 @@
+import {
+  to = proxmox_lxc.alpine-lxc-template
+  id = "hypervisor/lxc/8000"
+}
+
+# __generated__ by OpenTofu
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by OpenTofu
+resource "proxmox_lxc" "alpine-lxc-template" {
+  lifecycle {
+    ignore_changes = [ tags ]
+  }
+  arch                 = "amd64"
+  bwlimit              = 0
+  clone                = null
+  clone_storage        = null
+  cmode                = ""
+  console              = true
+  cores                = 1
+  cpulimit             = 0
+  cpuunits             = 1024
+  description          = ""
+  force                = false
+  full                 = null
+  hagroup              = ""
+  hastate              = ""
+  hookscript           = ""
+  hostname             = "alpine-lxc-template"
+  ignore_unpack_errors = false
+  lock                 = ""
+  memory               = 512
+  nameserver           = ""
+  onboot               = false
+  ostemplate           = null
+  ostype               = "alpine"
+  password             = null # sensitive
+  pool                 = null
+  protection           = false
+  restore              = false
+  searchdomain         = ""
+  ssh_public_keys      = null
+  start                = false
+  startup              = ""
+  swap                 = 512
+  tags                 = ""
+  target_node          = null
+  template             = true
+  tty                  = 2
+  unique               = false
+  unprivileged         = true
+  vmid                 = 8000
+  rootfs {
+    acl       = false
+    quota     = false
+    replicate = false
+    ro        = false
+    shared    = false
+    size      = "8G"
+    storage   = ""
+  }
+}
---
+++ terraform/lxc_semaphore.tf
@@ -0,0 +1,38 @@
+resource "proxmox_lxc" "semaphore" {
+  lifecycle {
+    ignore_changes = [ tags ]
+  }
+  arch                 = "amd64"
+  # Commented because it would cause recreation. I applied first with those uncommented,
+  # then commented to edit some parameters.
+  # clone                = "alpine-lxc-template"       # ID of your template container
+  # full                 = true         # Full clone (independent copy)
+  cmode                = ""
+  console              = true
+  cores                = 1
+  cpuunits             = 1024
+  description          = ""
+  force                = false
+  hagroup              = ""
+  hastate              = ""
+  hookscript           = ""
+  hostname             = "semaphore"
+  memory               = 1024
+  onboot               = true
+  ostype               = "alpine"
+  swap                 = 512
+  start                = true
+  tags                 = ""
+  target_node          = "hypervisor"
+  tty                  = 2
+  unprivileged         = true
+  features {
+    nesting = true
+  }
+
+  rootfs {
+    size  = "8G"
+    storage = ""
+  }
+  
+}
